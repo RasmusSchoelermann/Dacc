f13625930d73b5dd7bfe854689f8c33e1c1cd25a
Rasmus Schölermann <rasmus.schoelermann@web.de>
2019-03-11 22:32:18 +0100

    Build to check Multiplayer.

---
+++ Dacc/Assets/Scripts/CostumNetworkManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager
+{
+    [SerializeField] private GameObject sceneCamera;
+    [SerializeField] private GameObject reticuleCanvas;
+
+    public override void OnStartClient(NetworkClient client)
+    {
+        HideSceneCamera();
+        ShowReticuleCanvas();
+    }
+
+    public override void OnStartHost()
+    {
+        HideSceneCamera();
+        ShowReticuleCanvas();
+    }
+
+    public override void OnStopClient()
+    {
+        ShowSceneCamera();
+        HideReticuleCanvas();
+    }
+
+    public override void OnStopHost()
+    {
+        ShowSceneCamera();
+        HideReticuleCanvas();
+    }
+
+    private void HideSceneCamera()
+    {
+        if (sceneCamera)
+            sceneCamera.SetActive(false);
+    }
+
+    private void ShowSceneCamera()
+    {
+        if (sceneCamera)
+            sceneCamera.SetActive(true);
+    }
+
+    private void HideReticuleCanvas()
+    {
+        if (reticuleCanvas)
+            reticuleCanvas.SetActive(false);
+    }
+
+    private void ShowReticuleCanvas()
+    {
+        if (reticuleCanvas)
+            reticuleCanvas.SetActive(true);
+    }
+}
---
+++ Dacc/Assets/Scripts/NetworkedPlayerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkedPlayerScript : NetworkBehaviour
+{
+    public UnityStandardAssets.Characters.FirstPerson.FirstPersonController fpsController;
+    public Camera fpsCamera;
+    public AudioListener audioListener;
+
+    public override void OnStartLocalPlayer()
+    {
+        fpsController.enabled = true;
+        fpsCamera.enabled = true;
+        audioListener.enabled = true;
+
+        gameObject.name = "LOCAL Player";
+
+        base.OnStartLocalPlayer();
+    }
+}
