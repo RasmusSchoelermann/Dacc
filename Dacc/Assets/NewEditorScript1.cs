f53373afe0418f45f1a3594351941bf19635e103
Rasmus Schölermann <rasmus.schoelermann@web.de>
2019-03-13 00:58:00 +0100

    Deleted most of the Standart Assets.

--- Dacc/Assets/NewEditorScript1.cs
+++ Dacc/Assets/NewEditorScript1.cs
@@ -6,9 +6,4 @@ public class Unit : ScriptableObject
     public int Team;
     public int ArrayX;
     public int ArrayY;
-    [MenuItem("Tools/MyTool/Do It in C#")]
-    static void DoIt()
-    {
-        EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
-    }
 }
