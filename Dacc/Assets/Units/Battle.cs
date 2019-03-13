11a58e506d632c7aa39abb29a031eef562a95462
Nick Buchhorn <>
2019-03-13 19:20:51 +0100

    Startet BattleSystem;

--- Dacc/Assets/Units/Battle.cs
+++ Dacc/Assets/Units/Battle.cs
@@ -4,21 +4,49 @@ using UnityEngine;
 
 public class Battle : MonoBehaviour
 {
-    GameObject[,] PlanesArray;
+    public GameObject[,] PlanesArray;
+    Unit[,] BattleBoard;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void startbattle(Unit[,] board)
+    public void startbattle(Unit[,] boardref)
+    {
+        BattleBoard = boardref;
+        foreach (Unit U in BattleBoard)
+        {
+            if (U != null)
+            {
+                U.ArrayX = U.GetComponent<BoardLocation>().Bx;
+                U.ArrayY = U.GetComponent<BoardLocation>().By;
+                U.startAi(this, PlanesArray);
+            }
+
+        }
+    }
+
+    public void MoveUnit(Unit Unittomove, int X,int Y)
+    {
+        Vector3 newposition = PlanesArray[X, Y].gameObject.transform.position;
+        newposition.y = Unittomove.gameObject.transform.position.y;
+        Unittomove.gameObject.transform.position = newposition;
+        BattleBoard[X, Y] = Unittomove;
+        BattleBoard[Unittomove.ArrayX, Unittomove.ArrayX] = null;
+        Unittomove.ArrayX = X;
+        Unittomove.ArrayY = Y;
+
+    }
+
+    public void RangeCheck(Unit Unit)
     {
 
     }
