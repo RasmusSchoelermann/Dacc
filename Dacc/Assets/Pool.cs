08e351f3e0e84503c2b4df5b99840b3bf484b1ac
Nick Buchhorn <>
2019-03-14 20:51:10 +0100

    Added: PoolScript;

---
+++ Dacc/Assets/Pool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool : MonoBehaviour
+{
+    [System.Serializable]
+    public class PoolRefs
+    {
+        public GameObject[] Units;
+    }
+    public PoolRefs[] Seltenheit;
+
+
+    [System.Serializable]
+    public class Zahl
+    {
+        public int[] Units = new int[2];
+    }
+    public Zahl[] UnitQuantity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
--- Dacc/Assets/Units/Unit.cs
+++ Dacc/Assets/Units/Unit.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     public int Team;
+    public int id;
 
     public int ArrayX;
     public int ArrayY;
